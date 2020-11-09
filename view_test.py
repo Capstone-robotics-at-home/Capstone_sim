06fc78540a3cf5a8040aa111b2be00e36ddbceaa
ZionDeng <569598401@qq.com>
2020-11-09 17:23:28 +0800

    initiate this folder

---
+++ view_test.py
@@ -0,0 +1,10 @@
+from mujoco_py import load_model_from_path, MjSim, MjViewer
+
+model_path = "hello.xml"
+model = load_model_from_path(model_path)
+sim = MjSim(model)
+viewer = MjViewer(sim)
+
+for i in range(15000):
+    sim.step()
+    viewer.render()
